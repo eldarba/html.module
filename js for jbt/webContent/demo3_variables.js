653b523ad047161992fc3d156ff852845e5f5767
eldar <eldar@DESKTOP-LALDJBT>
2019-03-15 05:50:29 +0200

    c1

---
+++ js for jbt/webContent/demo3_variables.js
@@ -0,0 +1,19 @@
+// declare and initialize a variable as number
+var x = 5;
+console.log(x);
+
+// assign the variable another number
+x = 62.36;
+console.log(x);
+
+// assign the variable a boolean value
+x = true;
+console.log(x);
+
+// assign the variable a String value
+x = "this is text";
+console.log(x);
+
+// assign the variable another String value
+x = 'some other text';
+console.log(x);
